ddb74912e4955b5a26f2eb6fc8af3a7f963be8ad
Atuar Rahman <atuar.avis@gmail.com>
2018-07-30 14:17:00 +0600

    Added post requested method configured

--- src/main/java/com/mealmanagement/service/TopicService.java
+++ src/main/java/com/mealmanagement/service/TopicService.java
@@ -3,17 +3,18 @@ package com.mealmanagement.service;
 import com.mealmanagement.model.Topic;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
 
-     private List<Topic> topics = Arrays.asList(
+     private List<Topic> topics = new ArrayList<>( Arrays.asList(
             new Topic("1", "Framework","J2EE Application Development"),
             new Topic("2", "Framework","J2EE Application Development"),
             new Topic("3", "Framework","J2EE Application Development")
-    );
+    ));
 
     public List<Topic> getAllTopics() {
         return topics;
@@ -21,5 +22,9 @@ public class TopicService {
 
     public Topic getTopic(String id){
         return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
     }
 }
